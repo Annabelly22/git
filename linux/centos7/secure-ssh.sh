7ea28468738d3ae14cd68735e02ea293bb5e5f5e
Annabelly22 <annabel.otutu@mymail.champlain.edu>
2023-02-22 18:36:05 +0000

    updated shell script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,2 +1,7 @@
 #secure-ssh.sh
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp git/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
